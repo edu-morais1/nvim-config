ed6b68e253b4b0544bd40f8327dde6b16da457b9
edu-morais1 <vinicius_eduardo19@outlook.com>
2026-07-20 15:54:42 -0300

    refactor: fix colorscheme option, move cmp config to plugin spec

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,1 +1,10 @@
 vim.g.lazyvim_colorscheme = "catppuccin"
+
+local opt = vim.opt
+opt.relativenumber = true
+opt.scrolloff = 8
+opt.clipboard = "unnamedplus"
+opt.undofile = true
+opt.wrap = false
+opt.splitright = true
+opt.splitbelow = true
